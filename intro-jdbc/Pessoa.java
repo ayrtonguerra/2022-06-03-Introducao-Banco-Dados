5b6523da031c9589a69a97d8b35575d54850856a
Ayrton Rodrigues G <70025680+ayrtonguerra@users.noreply.github.com>
2022-06-03 11:39:20 -0300

    Commit inicial

---
+++ intro-jdbc/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    //Por questões de simplicidade, vamos violar o princípio de alta coesão, trazendo mais responsabilidades para a classe Pessoa, para facilitar um pouco o desenvolvimento do código para essa aula.
+    public void inserir(){
+
+    }
+}
